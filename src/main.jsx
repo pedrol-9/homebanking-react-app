969b25f5f47c79b86dee433f724917598845e144
Pedro Sanabria <psanabria999@gmail.com>
2024-06-19 10:32:00 -0500

    replacing main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import { Store }  from './redux/Store.js'
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <React.StrictMode>
+    <Provider store={Store}>
+        <App />
+        <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            transition={Bounce}
+        />
+    </Provider>
+    // </React.StrictMode>
+)
