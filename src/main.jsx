403a29f294b93eb4840154b76419f4b61c0ca873
Pedro Sanabria <psanabria999@gmail.com>
2024-06-19 10:36:26 -0500

    replacing store.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React, { StrictMode } from 'react'
+/* import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { Store }  from './redux/Store.js'
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-    <Provider store={Store}>
+    <Provider store={store}>
         <App />
         <ToastContainer
             position="top-right"
@@ -26,4 +25,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         />
     </Provider>
     // </React.StrictMode>
-)
+) */
