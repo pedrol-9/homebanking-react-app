da123153d5840ee835045df38affa52136dff849
Pedro Sanabria <psanabria999@gmail.com>
2024-06-19 10:17:19 -0500

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { Store } from './HOCs/Store.js'
+import Store  from './HOCs/Store.js'
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
